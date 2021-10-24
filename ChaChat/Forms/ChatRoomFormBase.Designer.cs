fd227ef17373ccdbaf878322212a6992bccf25c9
ShoseiAtsumi <40586356+ShoseiAtsumi@users.noreply.github.com>
2021-10-24 23:04:03 +0900

    プロジェクト ファイルを追加します。

---
+++ ChaChat/Forms/ChatRoomFormBase.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace ChaChat.Forms
+{
+    partial class ChatRoomFormBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChat = new MetroFramework.Controls.MetroTextBox();
+            this.btnExit = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            // 
+            // 
+            // 
+            this.txtChat.CustomButton.Image = null;
+            this.txtChat.CustomButton.Location = new System.Drawing.Point(412, 1);
+            this.txtChat.CustomButton.Name = "";
+            this.txtChat.CustomButton.Size = new System.Drawing.Size(341, 341);
+            this.txtChat.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtChat.CustomButton.TabIndex = 1;
+            this.txtChat.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtChat.CustomButton.UseSelectable = true;
+            this.txtChat.CustomButton.Visible = false;
+            this.txtChat.Lines = new string[0];
+            this.txtChat.Location = new System.Drawing.Point(23, 63);
+            this.txtChat.MaxLength = 32767;
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.PasswordChar = '\0';
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtChat.SelectedText = "";
+            this.txtChat.SelectionLength = 0;
+            this.txtChat.SelectionStart = 0;
+            this.txtChat.ShortcutsEnabled = true;
+            this.txtChat.Size = new System.Drawing.Size(754, 343);
+            this.txtChat.TabIndex = 0;
+            this.txtChat.UseSelectable = true;
+            this.txtChat.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtChat.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(702, 412);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "終了";
+            this.btnExit.UseSelectable = true;
+            // 
+            // ChatRoomFormBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtChat);
+            this.Name = "ChatRoomFormBase";
+            this.Text = "ChatRoomForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        protected MetroFramework.Controls.MetroTextBox txtChat;
+        protected MetroFramework.Controls.MetroButton btnExit;
+    }
+}
---
+++ ChaChat/Forms/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Net;
+using UtilityTCP;
+
+namespace ChaChat
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateRoom_Click(object sender, EventArgs e)
+        {
+            UtilityTCP.Server server1 = new Server(IPAddress.Any, 21212);
+            var chatRoom =  new ChaChat.Forms.ChatRoomForServerForm(server1);
+            chatRoom.ShowDialog();
+        }
+
+        private void btnJoinRoom_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
